b4eb5f5d50993a09243d4322f430f0c5d5dcfede
ybm1968 <yby1969@naver.com>
2023-09-26 14:37:03 +0900

    test

---
+++ JavaFX_Board/src/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("team4");
+		System.out.println("team4");
+		System.out.println("team4");
+		System.out.println("team4");
+		
+	}
+}
